b83571d4b04b4a7cb3c26c910182e6d201a2364a
Scott Xu <jianx2@cisco.com>
2018-12-04 15:03:34 +0800

    add razplus part

--- razplusfreetrial.py
+++ razplusfreetrial.py
@@ -1,11 +1,28 @@
 #import libs
 import threading
+import random, string
 import requests, pyquery
 
 #get Email address from 10 Minutes Mail https://10minutemail.com
 mail_url = "https://10minutemail.com"
-pq= pyquery.PyQuery(requests.get("https://10minutemail.com").text)
+rs = requests.Session()
+mail_final_url = rs.get(mail_url).url
+r_mail = rs.get(mail_final_url).text 
+pq= pyquery.PyQuery(r_mail)
 mail_address = pq('#mailAddress').attr("value")
 print(mail_address)
-
+print(mail_final_url)
 #register in raz-plus
+username = mail_address.split("@")[0]
+fname = ''.join(random.sample(string.ascii_letters, 4))
+lname = ''.join(random.sample(string.ascii_letters, 4))
+print(fname, lname)
+
+headers = {"Referer": "https://accounts.learninga-z.com/accountsweb/marketing/trial.do?campaign=trialbtnnxtologoRP"}
+payload = {"mdrQuery.stateId": 0, "mdrQuery.mdrType": "public", "international": "true", \
+           "mdrQuery.useMdr": "false", "campaignName": "trialbtnnxtologoRP", "org": "HHJJKK", \
+           "customerType": "new", "usageType": "classroom", "firstName": fname, \
+           "lastName": lname, "zip": 99999, "countryId": 98, "email": mail_address, \
+           "mdrQuery.freeFormOrgName": "HHJJKK", "newUserUsername": username}
+r_razplus = requests.post("https://accounts.learninga-z.com/accountsweb/marketing/trial.do", data=payload, headers=headers)
+print(r_razplus.text)
