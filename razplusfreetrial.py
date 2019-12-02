723186e986cc5e0c5bc5978b0f0809443e7dde04
Scott Xu <fgsoap@gmail.com>
2019-12-02 21:27:11 +0800

    add threading

--- razplusfreetrial.py
+++ razplusfreetrial.py
@@ -1,73 +1,109 @@
 """This is for razplus-trial"""
 import random
 import string
+import sys
 import time
-import requests
+from queue import Queue
+from threading import Thread
+
 import pyquery
+import requests
 
-# Get Email address from TEMPAIL https://tempail.com/en/
 
-MAIL_URL = "https://tempail.com/en/"
-RS_TEMPAIL = requests.Session()
-MAIL_FINAL_URL = RS_TEMPAIL.get(MAIL_URL).url
-R_MAIL = RS_TEMPAIL.get(MAIL_FINAL_URL).text
-PQ = pyquery.PyQuery(R_MAIL)
-MAIL_ADDRESS = PQ('#eposta_adres').attr("value")
+def get_email(q):
+    # Get Email address
+    print("in get_email ...")
+    mail_url = "https://10minutemail.com/10MinuteMail/index.html"
+    rs_tmpmail = requests.Session()
+    mail_final_url = rs_tmpmail.get(mail_url).url
+    r_mail = rs_tmpmail.get(mail_final_url).text
+    pq = pyquery.PyQuery(r_mail)
+    mail_address = pq('#mailAddress').attr("value")
+    q.put((mail_address, rs_tmpmail))
 
-# Register in raz-plus
 
-USERNAME = MAIL_ADDRESS.split("@")[0]
-F_NAME = ''.join(random.sample(string.ascii_letters, 4))
-L_NAME = ''.join(random.sample(string.ascii_letters, 4))
+def check_email(q):
+    # Check Email
+    print("in check_email ...")
+    rs_tmpmail = q.get()
+    start_time = time.time()
+    while True:
+        mail_final_url = rs_tmpmail.get("https://10minutemail.com/10MinuteMail/index.html").url
+        r_mail = rs_tmpmail.get(mail_final_url).text
+        pq = pyquery.PyQuery(r_mail)
+        mail = pq('#mail-clock-wrapper > div.mail-notification.unread')
+        print(mail)
+        time.sleep(1)
+        stop_time = time.time()
+        if (stop_time - start_time) > 120:
+            break
 
-RS_RAZPLUS = requests.Session()
-RS_RAZPLUS.get(
-    "https://accounts.learninga-z.com/accountsweb/marketing/trial.do?campaign=trialbtnnxtologoRP"
-)
-HEADERS = {
-    "Content-Type":
-    "application/x-www-form-urlencoded",
-    "Connection":
-    "keep-alive",
-    "Upgrade-Insecure-Requests":
-    "1",
-    "Host":
-    "accounts.learninga-z.com",
-    "Accept":
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Origin":
-    "https://accounts.learninga-z.com",
-    "Referer":
-    "https://accounts.learninga-z.com/accountsweb/marketing/trial.do?campaign=trialbtnnxtologoRP"
-}
-PAYLOAD = {
-    "mdrQuery.stateId": 0,
-    "mdrQuery.mdrType": "public",
-    "international": "true",
-    "mdrQuery.useMdr": "false",
-    "campaignName": "trialbtnnxtologoRP",
-    "org": "HHJJKK",
-    "customerType": "new",
-    "usageType": "classroom",
-    "firstName": F_NAME,
-    "lastName": L_NAME,
-    "zip": 99999,
-    "countryId": 98,
-    "email": MAIL_ADDRESS,
-    "occupation": 39,
-    "mdrQuery.freeFormOrgName": "HHJJKK",
-    "newUserUsername": USERNAME
-}
-response = RS_RAZPLUS.post(
-    "https://accounts.learninga-z.com/accountsweb/marketing/trial.do",
-    data=PAYLOAD,
-    headers=HEADERS)
-if "An email has been sent" in response.text:
-    print "Registed in RAZPLUS Successfully!"
-else:
-    print "Registed in RAZPLUS failed!"
 
-time.sleep(120)
+def register_razplus(q):
+    # Register in raz-plus
+    print("in register_razplus ...")
+    mail_address, rs_tmpmail = q.get()
+    username = mail_address.split("@")[0]
+    f_name = ''.join(random.sample(string.ascii_letters, 4))
+    l_name = ''.join(random.sample(string.ascii_letters, 4))
+    rs_razplus = requests.Session()
+    rs_razplus.get(
+        "https://accounts.learninga-z.com/accountsweb/marketing/trial.do?campaign=trialbtnnxtologoRP"
+    )
+    headers = {
+        "Content-Type":
+            "application/x-www-form-urlencoded",
+        "Connection":
+            "keep-alive",
+        "Upgrade-Insecure-Requests":
+            "1",
+        "Host":
+            "accounts.learninga-z.com",
+        "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Origin":
+            "https://accounts.learninga-z.com",
+        "Referer":
+            "https://accounts.learninga-z.com/accountsweb/marketing/trial.do?campaign=trialbtnnxtologoRP"
+    }
+    payload = {
+        "mdrQuery.stateId": 0,
+        "mdrQuery.mdrType": "public",
+        "international": "true",
+        "mdrQuery.useMdr": "false",
+        "campaignName": "trialbtnnxtologoRP",
+        "org": "HHJJKK",
+        "customerType": "new",
+        "usageType": "classroom",
+        "firstName": f_name,
+        "lastName": l_name,
+        "zip": 99999,
+        "countryId": 98,
+        "email": mail_address,
+        "occupation": 39,
+        "mdrQuery.freeFormOrgName": "HHJJKK",
+        "newUserUsername": username
+    }
+    response = rs_razplus.post(
+        "https://accounts.learninga-z.com/accountsweb/marketing/trial.do",
+        data=payload,
+        headers=headers)
+    if "An email has been sent" in response.text and mail_address in response.text:
+        print("Registed in RAZPLUS Successfully!")
+        q.put(rs_tmpmail)
+    else:
+        print("Registed in RAZPLUS failed!")
+        sys.exit()
 
-RR_MAIL = RS_TEMPAIL.get(MAIL_FINAL_URL).text
-print RR_MAIL.text
+
+if __name__ == "__main__":
+    q = Queue()
+    t1 = Thread(target=get_email, args=(q,), name="get_email")
+    t2 = Thread(target=register_razplus, args=(q,), name="razplus")
+    t3 = Thread(target=check_email, args=(q,), name="check_email")
+    t1.start()
+    t1.join()
+    t2.start()
+    t2.join()
+    t3.start()
+    t3.join()
